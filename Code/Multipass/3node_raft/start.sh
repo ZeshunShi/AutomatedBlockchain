2deacb9ecd165b01586d38a5a5e039dd67b7a777
ZeshunShi <44780304+ZeshunShi@users.noreply.github.com>
2020-04-18 02:13:08 +0200

    docker

--- Code/Multipass/3node_raft/start.sh
+++ Code/Multipass/3node_raft/start.sh
@@ -7,7 +7,9 @@ multipass launch --name monitor 16.04
 
 // start consensus protocal and genesis block on node0
 sawset genesis --key /etc/sawtooth/keys/validator.priv -o config-genesis.batch
-sawset proposal create --key /etc/sawtooth/keys/validator.priv -o config-consensus.batch sawtooth.consensus.algorithm.name=raft sawtooth.consensus.algorithm.version=0.1.1 sawtooth.consensus.raft.peers=[\"03ad44bc90ea8ebcd1476e31e06c2693c129cd1cbed32fd20e260e3ae30b5b8fb4\",\"029f9d8a1f0c2b25fb82c6e6728e26b8b97a1a85e972865605d744abbc4afaefdb\",\"026b6cc6b49f1f8539b8d40470d5eec56d1cdba603650597e832ec423f189ff30c\"]
+
+
+sawset proposal create --key /etc/sawtooth/keys/validator.priv -o config-consensus.batch sawtooth.consensus.algorithm.name=raft sawtooth.consensus.algorithm.version=0.1.1 sawtooth.consensus.raft.peers=[\"02edb7c28f93cef054ecfeca145a4781183862b0e68a67ca9a8375819cb1ef3c63\",\"0283ea2064b2a0023fadceb9ccf12727bb3bea6e9a920a893fde880f3a53ded7e3\",\"03dae9d25d3926fe520522441466e207a7937ff49e1924ec59e234cb038280e34d\"]
 sawset proposal create --key /etc/sawtooth/keys/validator.priv -o raft-settings.batch sawtooth.consensus.raft.heartbeat_tick=2 sawtooth.consensus.raft.election_tick=20 sawtooth.consensus.raft.period=3500
 sawadm genesis config-genesis.batch config-consensus.batch raft-settings.batch
 
@@ -19,6 +21,14 @@ systemctl start sawtooth-identity-tp.service
 systemctl start sawtooth-intkey-tp-python.service
 systemctl start sawtooth-validator.service
 systemctl start sawtooth-raft-engine.service
+
+// stop service on node0, node1, and node2
+systemctl stop sawtooth-rest-api.service
+systemctl stop sawtooth-settings-tp.service
+systemctl stop sawtooth-identity-tp.service
+systemctl stop sawtooth-intkey-tp-python.service
+systemctl stop sawtooth-validator.service
+systemctl stop sawtooth-raft-engine.service
 
 // node IP address
 node0                   Running           192.168.64.4     Ubuntu 16.04 LTS
